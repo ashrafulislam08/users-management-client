c4ba82d1be8a34085901d080d8c38669db9bc488
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-11-24 12:00:17 +0600

    Added user form to send user data to server and get

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,42 @@ function App() {
     fetch("http://localhost:5000/users")
       .then((res) => res.json())
       .then((data) => setUsers(data));
-  }, []);
+  }, [users]);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+      });
+  };
+
   return (
     <>
       <h1>Users Management System</h1>
       <h2>Total Users: {users.length}</h2>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" />
+        <br />
+        <input type="email" name="email" placeholder="Email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+
       <ul>
         {users.map((user) => (
           <li key={user.id}>
